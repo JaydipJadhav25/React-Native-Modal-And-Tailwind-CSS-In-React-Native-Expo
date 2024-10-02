2db15a48037f22fac09701affb1d8c362f20c24f
jaydipjadhav <jaydipdjadhav>
2024-10-02 14:45:16 +0530

    use react-native-modal package

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { Text, View , Modal, Button } from "react-native";
-
+import { Text, View ,  Button } from "react-native";
+import {ReactNativeModal} from "react-native-modal";
 
 export default function Index() {
 
@@ -16,17 +16,17 @@ export default function Index() {
 
       }}
     >
-     <View style={{ backgroundColor : "red" , width : 100 , height : 80 , justifyContent : "center" }}> 
+     {/* <View style={{ backgroundColor : "red" , width : 100 , height : 80 , justifyContent : "center" }}> 
       <Text>Jaydip Dhananjay Jadhav </Text>
       
       <Button title="click" onPress={() => setvisiable(!visiable)}/>
 
-     </View>
+     </View> */}
 
 
      {/* model */}
 
-     <Modal  visible={visiable}  animationType="slide" 
+     {/* <Modal  visible={visiable}  animationType="slide" 
     //  transparent
       >
 
@@ -47,8 +47,59 @@ export default function Index() {
      </View>
        </View>
          
-        </Modal>
+        </Modal> */}
       
+
+
+       {/* use react-native-modal */}
+
+
+      <Button title="click" onPress={() => setvisiable(!visiable)}/>
+        
+      <ReactNativeModal isVisible={visiable} 
+      animationIn={"shake"}
+      animationOut={"flash"}
+      animationInTiming={900}
+      animationOutTiming={900}
+      style={{
+        // flex : 1,
+        backgroundColor : "red"
+      }}
+      onBackdropPress={() => setvisiable(!visiable)}
+      >
+        
+        <View
+        style={{
+          height : 300,
+          width : 500,
+           backgroundColor : "green",
+           borderRadius : 50,
+           justifyContent : "center",
+           alignItems : "center"
+
+
+        }}
+        >
+
+      <Button title="click" onPress={() => setvisiable(!visiable)}/>
+
+        <View
+        style={{
+          backgroundColor : "black",
+          width : 300,
+          height : 200,
+        
+        }}
+        />
+
+        </View>
+
+      </ReactNativeModal>  
+
+
+
+
+
 
     </View>
   );
